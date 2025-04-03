e6aa239216db43b490cc9d397771a3c7ec27e461
eldeldani <52008785+eldeldani@users.noreply.github.com>
2025-04-03 14:34:50 +0200

    Update sap_directories_housekeeping.sh - final

--- sap_directories_housekeeping.sh
+++ sap_directories_housekeeping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Exit code
-exit_code=0
+overall_exit_status=0
 # Redirect everything to log file
 current_date=$(date +%Y-%m-%d)
 exec >> /tmp/sap_directories_housekeeping_$current_date.log 2>&1
@@ -13,6 +13,7 @@ exec >> /tmp/sap_directories_housekeeping_$current_date.log 2>&1
 # zip_days: The script will zip anything newer than keep_days and older than zip_days
 keep_days=1825
 zip_days=730
+#zip_days=1780
 
 # Array declaration for the file patterns to cleanup, You can edit by extending the patterns:
 # i.e.: You wan to add the pattern *.gz you would append it to the end
@@ -47,16 +48,27 @@ done
 echo "======================================================================="
 echo "$(date): Script started in $HOSTNAME"
 if [ "$1" != "execute" ]; then
-        echo -e "$(date): Script being executed in test mode, only listing, not deleting"
+        echo -e "$(date): Script being executed in TEST mode."
 else
-        echo -e "$(date): Script being executed in EXECUTION mode, it will delete."
+        echo -e "$(date): Script being executed in EXECUTION mode."
 fi
 echo "$(date): SAP SIDs to analyze: ${sid_array[@]}"
 echo "$(date): The following file patterns will be used: ${file_pattern_array[@]}"
 echo "$(date): Files older than $keep_days days will be deleted"
-echo "$(date): Files older than $zip_days and newer than $keep_days will be zipped"
-echo "$(date): Files newer than $zip_days will not be touched"
+echo "$(date): Files older than $zip_days days and newer than $keep_days days will be zipped"
+echo "$(date): Files newer than $zip_days days will not be touched"
 
+# Check for available compression tools
+if command -v gzip &> /dev/null; then
+    COMPRESS_TOOL="gzip"
+elif command -v bzip2 &> /dev/null; then
+    COMPRESS_TOOL="bzip2"
+elif command -v xz &> /dev/null; then
+    COMPRESS_TOOL="xz"
+else
+    echo -e "$(date): No suitable compression tool found."
+    exit 1
+fi
 
 #Before filesystem usage loop
 df -h |grep sap >> /tmp/before_fs_usage
@@ -77,39 +89,28 @@ do
         # Loop for every directory
         for dir in ${paths_array[@]}
         do
-                        if [ -d "$dir" ];then
-                                        #echo -e "$(date): Working on directory: $dir"
-                                        for pattern in ${file_pattern_array[@]}
-                                        do
-                                                files_to_delete=$(find -L $dir -maxdepth 1 -name "$pattern" -mtime +$keep_days -type f)
-                                                if [ -n "$files_to_delete" ]; then
-                                                        if [ "$1" != "execute" ]; then
-                                                                echo -e "$(date): The following files would be deleted on directory: $dir for pattern: $pattern if not in test mode, only listing..."
-                                                                find -L $dir -maxdepth 1 -name "$pattern" -mtime +$keep_days -type f -printf '%TY-%Tm-%Td %p\n' | sort -rn
-                                                        else
-                                                                echo -e "$(date): The following files will be deleted on directory: $dir for pattern: $pattern"
-                                                                find -L $dir -maxdepth 1 -name "$pattern" -mtime +$keep_days -type f -printf '%TY-%Tm-%Td %p\n' | sort -rn
-                                                                echo -e "$(date): Deleting the files..."
-                                                                find -L $dir -maxdepth 1 -name "$pattern" -mtime +$keep_days -type f -delete
-                                                        fi
-                                                fi                                                
-                                        done
-                                        for pattern in ${file_pattern_array[@]}
-                                        do
-                                           zip_files_to_delete=$(find -L $dir -maxdepth 1 -name "*.gz" -mtime +$keep_days -type f)
-                                            if [ -n "$zip_files_to_delete" ]; then
-                                                    if [ "$1" != "execute" ]; then
-                                                                echo -e "$(date): The following zipped files would be deleted on directory: $dir  if not in test mode, only listing..."
-                                                                find -L $dir -maxdepth 1 -name "*.gz" -mtime +$keep_days -type f -printf '%TY-%Tm-%Td %p\n' | sort -rn
-                                                        else
-                                                                echo -e "$(date): The following zipped files will be deleted on directory: $dir"
-                                                                find -L $dir -maxdepth 1 -name "*.gz" -mtime +$keep_days -type f -printf '%TY-%Tm-%Td %p\n' | sort -rn
-                                                                echo -e "$(date): Deleting the zipped filesfiles..."
-                                                                find -L $dir -maxdepth 1 -name "*.gz" -mtime +$keep_days -type f -delete
-                                                        fi
-                                                fi 
-                                        done
-                        fi
+            if [ -d "$dir" ];then
+            #echo -e "$(date): Working on directory: $dir"
+                for pattern in ${file_pattern_array[@]}
+                    do
+                        files_to_delete=$(find -L $dir -maxdepth 1 -name "$pattern" -mtime +$keep_days -type f)
+                        if [ -n "$files_to_delete" ]; then
+                            if [ "$1" != "execute" ]; then
+                                echo -e "$(date): The following files would be deleted on directory: $dir for pattern: $pattern if not in test mode, only listing..."
+                                find -L $dir -maxdepth 1 -name "$pattern" -mtime +$keep_days -type f -printf '%TY-%Tm-%Td %p\n' | sort -rn
+                            else
+                                echo -e "$(date): The following files will be deleted on directory: $dir for pattern: $pattern"
+                                find -L $dir -maxdepth 1 -name "$pattern" -mtime +$keep_days -type f -printf '%TY-%Tm-%Td %p\n' | sort -rn
+                                echo -e "$(date): Deleting the files..."
+                                find -L $dir -maxdepth 1 -name "$pattern" -mtime +$keep_days -type f -dddelete
+                                if ! [[ $? -eq 0 ]]; then
+                                    echo -e "$(date): Error when deleting files"
+                                    overall_exit_status=1
+                                fi
+                            fi
+                        fi                                                
+                    done
+            fi
         done
 done
 
@@ -133,7 +134,7 @@ do
                                         #echo -e "$(date): Working on directory: $dir"
                                         for pattern in ${file_pattern_array[@]}
                                         do
-                                                files_to_zip=$(find -L $dir -maxdepth 1 -name "$pattern" -mtime +$zip_days -mtime -$keep_days -type f)
+                                                files_to_zip=$(find -L $dir -maxdepth 1 -name "$pattern" ! -name "*.gz" ! -name "*.bz2" ! -name "*.xz" -mtime +$zip_days -mtime -$keep_days -type f)
                                                 if [ -n "$files_to_zip" ]; then
                                                         if [ "$1" != "execute" ]; then
                                                                 echo -e "$(date): The following files would be zipped on directory: $dir for pattern: $pattern if not in test mode, only listing..."
@@ -141,8 +142,30 @@ do
                                                         else
                                                                 echo -e "$(date): The following files will be zipped on directory: $dir for pattern: $pattern"
                                                                 find -L $dir -maxdepth 1 -name "$pattern" -mtime +$zip_days -mtime -$keep_days -type f -printf '%TY-%Tm-%Td %p\n' | sort -rn
-                                                                echo -e "$(date): Zipping files..."
-                                                                find -L $dir -maxdepth 1 -name "$pattern" -mtime +$zip_days -mtime -$keep_days -type f gzip -9 {} \;
+                                                                echo -e "$(date): Zipping files..."                                                                
+                                                                case $COMPRESS_TOOL in
+                                                                gzip)
+                                                                    find -L $dir -maxdepth 1 -name "$pattern" -mtime +$zip_days -mtime -$keep_days -type f -exec gzip {} \;
+                                                                    if ! [[ $? -eq 0 ]]; then
+                                                                        echo -e "$(date): Error when zipping files"
+				                                                        overall_exit_status=1
+                                                                    fi
+                                                                    ;;
+                                                                bzip2)
+                                                                    find -L $dir -maxdepth 1 -name "$pattern" -mtime +$zip_days -mtime -$keep_days -type f -exec bzip2 {} \;
+                                                                    if ! [[ $? -eq 0 ]]; then
+                                                                        echo -e "$(date): Error when zipping files"
+				                                                        overall_exit_status=1
+                                                                    fi
+                                                                    ;;
+                                                                xz)
+                                                                    find -L $dir -maxdepth 1 -name "$pattern" -mtime +$zip_days -mtime -$keep_days -type f -exec xz {} \;
+                                                                    if ! [[ $? -eq 0 ]]; then
+                                                                        echo -e "$(date): Error when zipping files"
+				                                                        overall_exit_status=1
+                                                                    fi
+                                                                    ;;
+                                                                esac
                                                         fi
                                                 fi
                                         done
@@ -161,3 +184,6 @@ cat /tmp/after_fs_usage
 
 #cleanup temporary files
 rm /tmp/before_fs_usage /tmp/after_fs_usage
+
+#Perform exit
+exit $overall_exit_status
